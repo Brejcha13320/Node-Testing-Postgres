ac3144842de00dc0beb307d174815a093888912f
Jesus David Mejia <davidmejia-2000@outlook.com>
2024-07-13 14:33:31 -0500

    implementation ci with github actions for unit test

--- setupTests.ts
+++ setupTests.ts
@@ -1,5 +1,14 @@
 import { config } from "dotenv";
 
-config({
-  path: ".env.test",
-});
+const env = process.env.NODE_ENV || "develop";
+
+const envs = {
+  develop: ".env",
+  test: ".env.test",
+};
+
+if (envs[env]) {
+  config({
+    path: envs[env],
+  });
+}
